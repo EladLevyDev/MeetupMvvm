8c0739f97423bf176c63b8e81cc5c14a5acbd9a2
Elad Levy <sacredoon@gmail.com>
2019-12-25 17:44:02 +0200

    happyDay

--- app/src/main/java/com/elad/meetup/MainActivity.kt
+++ app/src/main/java/com/elad/meetup/MainActivity.kt
@@ -2,12 +2,22 @@ package com.elad.meetup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.LinearInterpolator
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.elad.meetup.adapter.CryptoCurrencyAdapter
+import com.elad.meetup.main.adapter.CardStackAdapter
+import com.elad.meetup.main.callback.SpotDiffCallback
+import com.elad.meetup.main.model.Spot
 import com.elad.meetup.viewmodel.CryptoCurrencyViewModel
 import com.elad.meetup.viewmodel.CryptoCurrencyViewModelFactory
 import dagger.android.AndroidInjection
@@ -15,9 +25,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 import com.elad.meetup.model.CryptoCurrency
 import com.elad.meetup.utils.Constants
 import com.elad.meetup.utils.toast
+import com.yuyakaido.android.cardstackview.*
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CardStackListener {
 
 
     @Inject
@@ -26,6 +37,9 @@ class MainActivity : AppCompatActivity() {
     private var cryptocurrenciesAdapter = CryptoCurrencyAdapter(ArrayList())
     private lateinit var cryptoCurrencyViewModel: CryptoCurrencyViewModel
 
+    private val manager by lazy { CardStackLayoutManager(this, this) }
+    private val adapter by lazy { CardStackAdapter(createSpots()) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,7 +47,9 @@ class MainActivity : AppCompatActivity() {
         initializeRecycler()
         initViewModelMethods()
         loadData()
+        setupCardStackView()
     }
+
 
     private fun initViewModelMethods() {
         cryptoCurrencyViewModel = ViewModelProviders.of(this, cryptoCurrencyViewModelFactory).get(
@@ -45,8 +61,8 @@ class MainActivity : AppCompatActivity() {
                 if (it != null) {
                     val position = cryptocurrenciesAdapter.itemCount
                     cryptocurrenciesAdapter.addCryptocurrencies(it)
-                    recycler.adapter = cryptocurrenciesAdapter
-                    recycler.scrollToPosition(position - Constants.LIST_SCROLLING)
+                  //  recycler.adapter = cryptocurrenciesAdapter
+                  //  recycler.scrollToPosition(position - Constants.LIST_SCROLLING)
                 }
             })
 
@@ -57,21 +73,242 @@ class MainActivity : AppCompatActivity() {
         })
 
         cryptoCurrencyViewModel.cryptocurrenciesLoader().observe(this, Observer<Boolean> {
-            if (it == false) progressBar.visibility = View.GONE
+          //  if (it == false) progressBar.visibility = View.GONE
         })
     }
 
     private fun initializeRecycler() {
         val gridLayoutManager = GridLayoutManager(this, 1)
         gridLayoutManager.orientation = RecyclerView.VERTICAL
-        recycler.apply {
+      /*  recycler.apply {
             setHasFixedSize(true)
             layoutManager = gridLayoutManager
+        }*/
+
+    }
+
+    private fun loadData() {
+        //progressBar.visibility = View.VISIBLE
+        cryptoCurrencyViewModel.loadCryptocurrencies(Constants.LIMIT, Constants.OFFSET)
+    }
+
+    override fun onCardDragging(direction: Direction, ratio: Float) {
+        Log.d("CardStackView", "onCardDragging: d = ${direction.name}, r = $ratio")
+    }
+
+    override fun onCardSwiped(direction: Direction) {
+        Log.d("CardStackView", "onCardSwiped: p = ${manager.topPosition}, d = $direction")
+        if (manager.topPosition == adapter.itemCount - 5) {
+            paginate()
         }
     }
 
-    private  fun loadData() {
-        progressBar.visibility = View.VISIBLE
-        cryptoCurrencyViewModel.loadCryptocurrencies(Constants.LIMIT, Constants.OFFSET)
+    override fun onCardRewound() {
+        Log.d("CardStackView", "onCardRewound: ${manager.topPosition}")
     }
+
+    override fun onCardCanceled() {
+        Log.d("CardStackView", "onCardCanceled: ${manager.topPosition}")
+    }
+
+    override fun onCardAppeared(view: View, position: Int) {
+        val textView = view.findViewById<TextView>(R.id.item_name)
+        Log.d("CardStackView", "onCardAppeared: ($position) ${textView.text}")
+    }
+
+    override fun onCardDisappeared(view: View, position: Int) {
+        val textView = view.findViewById<TextView>(R.id.item_name)
+        Log.d("CardStackView", "onCardDisappeared: ($position) ${textView.text}")
+    }
+
+
+    private fun setupCardStackView() {
+        initialize()
+    }
+
+    private fun setupButton() {
+        val skip = findViewById<View>(R.id.skip_button)
+        skip.setOnClickListener {
+            val setting = SwipeAnimationSetting.Builder()
+                .setDirection(Direction.Left)
+                .setDuration(Duration.Normal.duration)
+                .setInterpolator(AccelerateInterpolator())
+                .build()
+            manager.setSwipeAnimationSetting(setting)
+            cardStackView.swipe()
+        }
+
+        val rewind = findViewById<View>(R.id.rewind_button)
+        rewind.setOnClickListener {
+            val setting = RewindAnimationSetting.Builder()
+                .setDirection(Direction.Bottom)
+                .setDuration(Duration.Normal.duration)
+                .setInterpolator(DecelerateInterpolator())
+                .build()
+            manager.setRewindAnimationSetting(setting)
+            cardStackView.rewind()
+        }
+
+        val like = findViewById<View>(R.id.like_button)
+        like.setOnClickListener {
+            val setting = SwipeAnimationSetting.Builder()
+                .setDirection(Direction.Right)
+                .setDuration(Duration.Normal.duration)
+                .setInterpolator(AccelerateInterpolator())
+                .build()
+            manager.setSwipeAnimationSetting(setting)
+            cardStackView.swipe()
+        }
+    }
+
+    private fun initialize() {
+        manager.setStackFrom(StackFrom.None)
+        manager.setVisibleCount(3)
+        manager.setTranslationInterval(8.0f)
+        manager.setScaleInterval(0.95f)
+        manager.setSwipeThreshold(0.3f)
+        manager.setMaxDegree(20.0f)
+        manager.setDirections(Direction.HORIZONTAL)
+        manager.setCanScrollHorizontal(true)
+        manager.setCanScrollVertical(true)
+        manager.setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
+        manager.setOverlayInterpolator(LinearInterpolator())
+        cardStackView.layoutManager = manager
+        cardStackView.adapter = adapter
+        cardStackView.itemAnimator.apply {
+            if (this is DefaultItemAnimator) {
+                supportsChangeAnimations = false
+            }
+        }
+    }
+
+    private fun paginate() {
+        val old = adapter.getSpots()
+        val new = old.plus(createSpots())
+        val callback = SpotDiffCallback(old, new)
+        val result = DiffUtil.calculateDiff(callback)
+        adapter.setSpots(new)
+        result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun reload() {
+        val old = adapter.getSpots()
+        val new = createSpots()
+        val callback = SpotDiffCallback(old, new)
+        val result = DiffUtil.calculateDiff(callback)
+        adapter.setSpots(new)
+        result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun addFirst(size: Int) {
+        val old = adapter.getSpots()
+        val new = mutableListOf<Spot>().apply {
+            addAll(old)
+            for (i in 0 until size) {
+                add(manager.topPosition, createSpot())
+            }
+        }
+        val callback = SpotDiffCallback(old, new)
+        val result = DiffUtil.calculateDiff(callback)
+        adapter.setSpots(new)
+        result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun addLast(size: Int) {
+        val old = adapter.getSpots()
+        val new = mutableListOf<Spot>().apply {
+            addAll(old)
+            addAll(List(size) { createSpot() })
+        }
+        val callback = SpotDiffCallback(old, new)
+        val result = DiffUtil.calculateDiff(callback)
+        adapter.setSpots(new)
+        result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun removeFirst(size: Int) {
+        if (adapter.getSpots().isEmpty()) {
+            return
+        }
+
+        val old = adapter.getSpots()
+        val new = mutableListOf<Spot>().apply {
+            addAll(old)
+            for (i in 0 until size) {
+                removeAt(manager.topPosition)
+            }
+        }
+        val callback = SpotDiffCallback(old, new)
+        val result = DiffUtil.calculateDiff(callback)
+        adapter.setSpots(new)
+        result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun removeLast(size: Int) {
+        if (adapter.getSpots().isEmpty()) {
+            return
+        }
+
+        val old = adapter.getSpots()
+        val new = mutableListOf<Spot>().apply {
+            addAll(old)
+            for (i in 0 until size) {
+                removeAt(this.size - 1)
+            }
+        }
+        val callback = SpotDiffCallback(old, new)
+        val result = DiffUtil.calculateDiff(callback)
+        adapter.setSpots(new)
+        result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun replace() {
+        val old = adapter.getSpots()
+        val new = mutableListOf<Spot>().apply {
+            addAll(old)
+            removeAt(manager.topPosition)
+            add(manager.topPosition, createSpot())
+        }
+        adapter.setSpots(new)
+        adapter.notifyItemChanged(manager.topPosition)
+    }
+
+    private fun swap() {
+        val old = adapter.getSpots()
+        val new = mutableListOf<Spot>().apply {
+            addAll(old)
+            val first = removeAt(manager.topPosition)
+            val last = removeAt(this.size - 1)
+            add(manager.topPosition, last)
+            add(first)
+        }
+        val callback = SpotDiffCallback(old, new)
+        val result = DiffUtil.calculateDiff(callback)
+        adapter.setSpots(new)
+        result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun createSpot(): Spot {
+        return Spot(
+            name = "Yasaka Shrine",
+            city = "Kyoto",
+            url = "https://source.unsplash.com/Xq1ntWruZQI/600x800"
+        )
+    }
+
+    private fun createSpots(): List<Spot> {
+        val spots = ArrayList<Spot>()
+        spots.add(Spot(name = "Yasaka Shrine", city = "Kyoto", url = "https://source.unsplash.com/Xq1ntWruZQI/600x800"))
+        spots.add(Spot(name = "Fushimi Inari Shrine", city = "Kyoto", url = "https://source.unsplash.com/NYyCqdBOKwc/600x800"))
+        spots.add(Spot(name = "Bamboo Forest", city = "Kyoto", url = "https://source.unsplash.com/buF62ewDLcQ/600x800"))
+        spots.add(Spot(name = "Brooklyn Bridge", city = "New York", url = "https://source.unsplash.com/THozNzxEP3g/600x800"))
+        spots.add(Spot(name = "Empire State Building", city = "New York", url = "https://source.unsplash.com/USrZRcRS2Lw/600x800"))
+        spots.add(Spot(name = "The statue of Liberty", city = "New York", url = "https://source.unsplash.com/PeFk7fzxTdk/600x800"))
+        spots.add(Spot(name = "Louvre Museum", city = "Paris", url = "https://source.unsplash.com/LrMWHKqilUw/600x800"))
+        spots.add(Spot(name = "Eiffel Tower", city = "Paris", url = "https://source.unsplash.com/HN-5Z6AmxrM/600x800"))
+        spots.add(Spot(name = "Big Ben", city = "London", url = "https://source.unsplash.com/CdVAUADdqEc/600x800"))
+        spots.add(Spot(name = "Great Wall of China", city = "China", url = "https://source.unsplash.com/AWh9C-QjhE4/600x800"))
+        return spots
+    }
+
 }
